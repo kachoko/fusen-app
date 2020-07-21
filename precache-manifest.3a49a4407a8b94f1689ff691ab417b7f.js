548286c36346a7f0651cd58c936a26055b996c07
kachoko <aykato@enceladus.co.jp>
2020-07-21 22:17:51 +0900

    Updates

---
+++ precache-manifest.3a49a4407a8b94f1689ff691ab417b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ddd401e5d8a25f3b250d5b5a8c5dc7",
+    "url": "/fusen-app/index.html"
+  },
+  {
+    "revision": "63aa87c2807299bdd6c7",
+    "url": "/fusen-app/static/css/main.c2757d13.chunk.css"
+  },
+  {
+    "revision": "da2e8c4005f25933517c",
+    "url": "/fusen-app/static/js/2.258aaf37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fusen-app/static/js/2.258aaf37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63aa87c2807299bdd6c7",
+    "url": "/fusen-app/static/js/main.6b7ac5ce.chunk.js"
+  },
+  {
+    "revision": "4692dde7962ca80346b3",
+    "url": "/fusen-app/static/js/runtime-main.78e4de54.js"
+  },
+  {
+    "revision": "047bdfea396ebe72ccea480cbc5ef3cb",
+    "url": "/fusen-app/static/media/uzura.047bdfea.ttf"
+  }
+]);
